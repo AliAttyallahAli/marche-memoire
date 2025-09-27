5921e844837ed6d511e34e65e6d0fcb639fd33e5
ALI ATTYALLAH ALI <aliattyallahali@gmail.com>
2025-09-27 16:35:48 +0000

    ajoute le choix des transaction entre le numero de carte et l'address wa

--- src/app/(app)/transactions/page.tsx
+++ src/app/(app)/transactions/page.tsx
@@ -94,9 +94,9 @@ export default function P2PPage() {
                 name="recipient"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Clé de portefeuille du destinataire</FormLabel>
+                    <FormLabel>N° de carte ou clé de portefeuille du destinataire</FormLabel>
                     <FormControl>
-                      <Input placeholder="0x..." {...field} />
+                      <Input placeholder="Ex: 4022... ou 0x..." {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
